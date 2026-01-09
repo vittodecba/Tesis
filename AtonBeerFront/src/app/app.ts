676c0a22786326ebed9e20bb53dfbc6c6b0c58ab
Sb9 <santiagobruera1@gmail.com>
2026-01-09 02:40:37 -0300

    PBI 80: Funcionalidad cerrar sesion

--- AtonBeerFront/src/app/app.ts
+++ AtonBeerFront/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AtonBeerFront');
+  title = 'AtonBeerFront';
+
+  // Inyectamos el Router para poder navegar sin recargar la página
+  constructor(private router: Router) {}
+
+  logout() {
+    localStorage.removeItem('token');
+    // Usamos navigate en lugar de window.location
+    this.router.navigate(['/login']);
+  }
 }
