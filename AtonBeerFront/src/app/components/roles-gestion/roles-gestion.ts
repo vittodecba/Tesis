96350b82c51be2a3b60ff227b85f8c23405d67cd
Sb9 <santiagobruera1@gmail.com>
2026-01-11 03:19:38 -0300

    Creacion componente roles y rutas

---
+++ AtonBeerFront/src/app/components/roles-gestion/roles-gestion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-gestion',
+  imports: [],
+  templateUrl: './roles-gestion.html',
+  styleUrl: './roles-gestion.css'
+})
+export class RolesGestion {
+
+}
