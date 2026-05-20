45c7755569ff23a56ab0037ff0761060543c55be
valentin-alfaro <valentinalfaro51@gmail.com>
2026-05-20 10:53:15 -0300

    fix: incluir migracion de observaciones
    - Agrega observaciones persistidas en pedidos
    - Incluye migración para agregar Observaciones a Pedidos

---
+++ AtonBeerTesis/AtonBeerTesis.Infrastructure/Migrations/20260519221134_PedidoActualizado.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AtonBeerTesis.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PedidoActualizado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Observaciones",
+                table: "Pedidos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Observaciones",
+                table: "Pedidos");
+        }
+    }
+}
